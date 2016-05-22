8ca8db89e6e5f9503fee09c1f35b7c213b6ad3db
Mat Byczkowski <matb@compose.io>
2016-05-21 21:55:46 -0700

    get logo from each business
    - still reference placeholders for now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,83 +8,109 @@ class HomeController < ApplicationController
     @businesses ||= [
       {
         name: "Aedan Fermented Foods",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Alicia’s Tamales Los Mayas",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "AйDa Piroshki",
+        logo: "business_placeholder_logo.png",
         address: "331 Cortland St, SF",
         coordinates: [[37.739307, -122.418002]]
       },
       {
         name: "Bini’s Kitchen",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Chiefo's Kitchen",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Crumble & Whisk",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "D’Maize",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Delicioso Creperie",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Don Bugito, Prehispanic Snackeria",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Eji’s Ethiopian",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "El Buen Comer",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "El Pipila",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Estrellita’s Snacks",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Hella Vegan Eats",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Kaleidoscope Living Foods",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Love and Hummus",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Minnie Bell's Soul Movement",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Nyum Bai",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Peaches Patties",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Reem's",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Saint & Olive",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Sal De Vida",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Sugarfoot Grits",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Sweets Collection",
+        logo: "business_placeholder_logo.png"
       },
       {
         name: "Taino’s Puerto Rican Food",
+        logo: "business_placeholder_logo.png"
       },
       {
-        name: "Zepeda Foods"
+        name: "Zepeda Foods",
+        logo: "business_placeholder_logo.png"
       }
     ]
   end
