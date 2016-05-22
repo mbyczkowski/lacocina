4e385830d9feb762b801499434f2038484bc55c9
Mat Byczkowski <matb@compose.io>
2016-05-21 20:50:31 -0700

    bring Google Maps back

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,95 @@
 class HomeController < ApplicationController
-  def show
-    @businesses = [
-      "Aedan Fermented Foods", "Alicia’s Tamales Los Mayas", "AйDa Piroshki",
-      "Bini’s Kitchen", "Chiefo's Kitchen", "Crumble & Whisk", "D’Maize",
-      "Delicioso Creperie", "Don Bugito, Prehispanic Snackeria", "Eji’s Ethiopian",
-      "El Buen Comer", "El Pipila", "Estrellita’s Snacks", "Hella Vegan Eats",
-      "Kaleidoscope Living Foods", "Love and Hummus", "Minnie Bell's Soul Movement",
-      "Nyum Bai", "Peaches Patties", "Reem's", "Saint & Olive", "Sal De Vida",
-      "Sugarfoot Grits", "Sweets Collection", "Taino’s Puerto Rican Food", "Zepeda Foods"
+
+  helper_method :businesses, :businesses_with_locations
+
+  private
+
+  def businesses
+    @businesses ||= [
+      {
+        name: "Aedan Fermented Foods",
+      },
+      {
+        name: "Alicia’s Tamales Los Mayas",
+      },
+      {
+        name: "AйDa Piroshki",
+        address: "331 Cortland St, SF",
+        coordinates: [[37.739307, -122.418002]]
+      },
+      {
+        name: "Bini’s Kitchen",
+      },
+      {
+        name: "Chiefo's Kitchen",
+      },
+      {
+        name: "Crumble & Whisk",
+      },
+      {
+        name: "D’Maize",
+      },
+      {
+        name: "Delicioso Creperie",
+      },
+      {
+        name: "Don Bugito, Prehispanic Snackeria",
+      },
+      {
+        name: "Eji’s Ethiopian",
+      },
+      {
+        name: "El Buen Comer",
+      },
+      {
+        name: "El Pipila",
+      },
+      {
+        name: "Estrellita’s Snacks",
+      },
+      {
+        name: "Hella Vegan Eats",
+      },
+      {
+        name: "Kaleidoscope Living Foods",
+      },
+      {
+        name: "Love and Hummus",
+      },
+      {
+        name: "Minnie Bell's Soul Movement",
+      },
+      {
+        name: "Nyum Bai",
+      },
+      {
+        name: "Peaches Patties",
+      },
+      {
+        name: "Reem's",
+      },
+      {
+        name: "Saint & Olive",
+      },
+      {
+        name: "Sal De Vida",
+      },
+      {
+        name: "Sugarfoot Grits",
+      },
+      {
+        name: "Sweets Collection",
+      },
+      {
+        name: "Taino’s Puerto Rican Food",
+      },
+      {
+        name: "Zepeda Foods"
+      }
     ]
+  end
+
+  def businesses_with_locations
+    businesses.select{ |b| b[:coordinates].present? }
   end
 end
