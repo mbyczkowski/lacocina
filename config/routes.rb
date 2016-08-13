048116b312dfc52115f0ec06df523648eb4a4e9a
mhgump <michaelgump.spam@gmail.com>
2016-08-13 17:27:23 -0400

    Fixes
    Fixed stuff and duplicated the image for the helper function

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
 
   resource :home, controller: :home
   resources :events, only: [:index, :show]
-  get ':name.:ext', to: redirect('/assets/%{name}.%{ext}'), constraints: { name: /.+/, ext: /(jpg|png|gif)/}
   get ':page' => 'pages#show'
 end
