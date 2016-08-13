809d11917ca64a71ee5d00f5586555090c4dcc61
mhgump <michaelgump.spam@gmail.com>
2016-08-13 15:53:50 -0400

    data-interchange
    Added new route for images not coming from image_tag()
    
    The images are all the same one right now for interchange but they can
    be changed later. I did test with different images to make sure this
    worked

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resource :home, controller: :home
   resources :events, only: [:index, :show]
+  get ':name.:ext', to: redirect('/assets/%{name}.%{ext}'), constraints: { name: /.+/, ext: /(jpg|png|gif)/}
   get ':page' => 'pages#show'
 end
