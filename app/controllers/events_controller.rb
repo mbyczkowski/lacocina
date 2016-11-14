8e0afcface59da8597ac2bc2c0a3a16056337ab4
Yotam Ginzburg <yoyo@machine3.local>
2016-11-14 13:36:45 -0800

    try to show events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,9 @@
 class EventsController < ApplicationController
   def index
-    render 'pages/coming_soon' and return unless work_in_progress?
     @events = Event.all
   end
 
   def show
-    render 'pages/coming_soon' and return unless work_in_progress?
     Event.find_by_id(params[:id])
   end
 end
