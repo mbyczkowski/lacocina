25ae1c8edb3a5c9924d31c70f7000fa6d8fabcec
Katie Thomas <katthomas@google.com>
2016-11-23 15:51:45 -0800

    Disable signup with config

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,11 @@ Rails.application.routes.draw do
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
-  get "/sign_up" => "clearance/users#new", as: "sign_up"
+  if Clearance.configuration.allow_sign_up?
+    get "/sign_up" => "clearance/users#new", as: "sign_up"
+  else
+    get "/sign_up" => "home#show"
+  end
   root 'home#show'
 
   resource :home, controller: :home
