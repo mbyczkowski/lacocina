3ca713ccd6de76000ccb791295fc8dd90d5562c6
Katie Thomas <katthomas@google.com>
2016-11-23 15:48:15 -0800

    Revert "Temporarily disable sign up"
    This reverts commit fc1e6ed6547d841d6303e43cb4b89f5d89adf897.

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
-  get "/sign_up" => "home#show"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
   root 'home#show'
 
   resource :home, controller: :home
