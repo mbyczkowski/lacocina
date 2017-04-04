2f4d7607f21eb7c70dc4f31e002c1dd8a4791394
Mat Byczkowski <mbyczkowski@gmail.com>
2017-04-03 21:06:13 -0700

    add lat/long to business model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119233655) do
+ActiveRecord::Schema.define(version: 20170404040553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20161119233655) do
     t.string   "logo_url"
     t.string   "website_url"
     t.string   "description"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "businesses", ["logo_url"], name: "index_businesses_on_logo_url", using: :btree
